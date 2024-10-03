718cec12fbeab8b0b16bea00f2e9a47caa16c499
Ryan0050 <lawyran0511@gmail.com>
2024-10-03 19:51:46 +0700

    changes

---
+++ Pages/About/about.js
@@ -0,0 +1,27 @@
+let currentSection = 0;
+const sections = document.querySelectorAll('section');
+const totalSections = sections.length;
+
+function scrollToSection(index) {
+    sections[index].scrollIntoView({ behavior: 'smooth' });
+}
+
+function handleScroll(event) {
+    event.preventDefault();
+
+    if (event.deltaY > 0) {
+        // Scroll down
+        if (currentSection < totalSections - 1) {
+            currentSection++;
+            scrollToSection(currentSection);
+        }
+    } else {
+        // Scroll up
+        if (currentSection > 0) {
+            currentSection--;
+            scrollToSection(currentSection);
+        }
+    }
+}
+
+window.addEventListener('wheel', handleScroll, { passive: false });
