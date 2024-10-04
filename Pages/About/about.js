13592b79dc0267f06f5d25915fb90a1ae65af048
Ryan0050 <lawyran0511@gmail.com>
2024-10-04 16:56:46 +0700

    edit about

--- Pages/About/about.js
+++ Pages/About/about.js
@@ -26,3 +26,35 @@
 
 // window.addEventListener('wheel', handleScroll, { passive: false });
 
+let bg2 = document.getElementById('bg2');
+let mission = document.getElementById('mission');
+let hill2 = document.getElementById('hill2');
+let hill3 = document.getElementById('hill3');
+let hill4 = document.getElementById('hill4');
+let hill5 = document.getElementById('hill5');
+
+
+window.addEventListener('scroll', () =>{
+    let value = window.scrollY;
+    mission.style.marginTop = value * 0.15 + 'vh';
+    bg2.style.marginTop = value * -0.1 + 'vh';
+    hill2.style.left = value * -0.3 + 'vh';
+    hill4.style.left = value * -0.3 + 'vh';
+    hill5.style.left = value * 0.5 + 'vh';
+});
+
+var lastScrollTop = 0;
+navbar = document.getElementById("navbar");
+window.addEventListener("scroll", function(){
+    var scrollTop = window.pageYOffset || doucment.documentElement.scrollTop;
+
+    if(scrollTop > lastScrollTop){
+        navbar.style.top = "-17vh";
+        
+    }
+    else{
+        console.log("masuk");
+        navbar.style.top= "0";
+    }
+    lastScrollTop = scrollTop;
+})
