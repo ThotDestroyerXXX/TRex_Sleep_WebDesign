885121bf6c45dd52860208bb0646949e5369cb06
Ryan0050 <lawyran0511@gmail.com>
2024-10-04 15:11:30 +0700

    help

--- Pages/About/about.js
+++ Pages/About/about.js
@@ -1,27 +1,28 @@
-let currentSection = 0;
-const sections = document.querySelectorAll('section');
-const totalSections = sections.length;
+// let currentSection = 0;
+// const sections = document.querySelectorAll('section');
+// const totalSections = sections.length;
 
-function scrollToSection(index) {
-    sections[index].scrollIntoView({ behavior: 'smooth' });
-}
+// function scrollToSection(index) {
+//     sections[index].scrollIntoView({ behavior: 'smooth' });
+// }
 
-function handleScroll(event) {
-    event.preventDefault();
+// function handleScroll(event) {
+//     event.preventDefault();
 
-    if (event.deltaY > 0) {
-        // Scroll down
-        if (currentSection < totalSections - 1) {
-            currentSection++;
-            scrollToSection(currentSection);
-        }
-    } else {
-        // Scroll up
-        if (currentSection > 0) {
-            currentSection--;
-            scrollToSection(currentSection);
-        }
-    }
-}
+//     if (event.deltaY > 0) {
+//         // Scroll down
+//         if (currentSection < totalSections - 1) {
+//             currentSection++;
+//             scrollToSection(currentSection);
+//         }
+//     } else {
+//         // Scroll up
+//         if (currentSection > 0) {
+//             currentSection--;
+//             scrollToSection(currentSection);
+//         }
+//     }
+// }
 
-window.addEventListener('wheel', handleScroll, { passive: false });
+// window.addEventListener('wheel', handleScroll, { passive: false });
+
